97b67c0788770b9a2615adffca01e0a697ea5e55
hmones <haythameyd@gmail.com>
2022-09-08 20:42:29 +0200

    First Commit:

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+</head>
+<body>
+@include('partials.navigation')
+<h1 class="text-3xl font-bold underline">
+    AkhbarMeter!
+</h1>
+</body>
+</html>
