f71fca88180ac1aa232ba096a65278aa1827b298
hmones <haythameyd@gmail.com>
2022-12-17 20:57:05 +0100

    Added methodology and about pages

--- resources/views/pages/video/index.blade.php
+++ resources/views/pages/video/index.blade.php
@@ -4,16 +4,10 @@
 @endsection
 @section('content')
     <div class="container max-h-full">
-        <div class="container hidden lg:flex flex-col w-full items-center justify-center mx-auto space-y-4 my-16">
-            <div class="flex flex-col">
-                <h1 class="text-5xl leading-10 font-extrabold tracking-tight">Videos</h1>
-            </div>
-            <div class="flex-initial w-1/3 flex-col">
-                <p class="text-lg text-center leading-6 font-normal text-gray-500">
-                    Simple text saying what the user should excpect by clicking on any of the news outlets.
-                </p>
-            </div>
-        </div>
+        @include('partials.page-header', [
+            'headline' => 'Videos',
+            'description' => 'Simple text saying what the user should excpect by clicking on any of the news outlets.'
+        ])
 
         <div class="container mb-10 space-y-10">
             @foreach($videos->chunk(3) as $rowVideos)
---
+++ resources/views/partials/support.blade.php
@@ -0,0 +1,20 @@
+<div class="bg-blue-700 text-white mt-4 py-20 mt-5">
+    <div class="container flex flex-col w-full items-center justify-center mx-auto space-y-4">
+        <div class="flex flex-col justify-content-center text-center">
+            <div class="text-4xl leading-10 font-extrabold tracking-tight">Support the project</div>
+            <div class="text-4xl leading-10 font-extrabold tracking-tight">Support us now!</div>
+        </div>
+        <div class="flex-initial w-2/5 flex-col">
+            <p class="text-center leading-6 font-normal">
+                We need your help to stay independent and to produce more videos and learning materials that would
+                benefit journalists and media consumers. Our project cannot continue without your support. We would
+                appreciate every cent you pay because this could help us become sustainable.
+            </p>
+        </div>
+        <div class="flex-col">
+            <button type="submit" class="mt-2 py-2.5 px-3 rounded bg-white hover:bg-gray-200 text-blue-600">Become
+                partner
+            </button>
+        </div>
+    </div>
+</div>
