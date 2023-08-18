ecd6eec588ab08bf4db600ed52f2abafe71e0ec3
Haytham Mones <haythameyd@gmail.com>
2023-08-18 17:49:18 +0200

    Make questions scrollable in review

--- resources/views/pages/review/edit.blade.php
+++ resources/views/pages/review/edit.blade.php
@@ -20,7 +20,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <form class="flex flex-col lg:flex-row" action="{{route('reviews.store')}}" method="POST" enctype="multipart/form-data">
                         @csrf
-                        <div class="flex flex-col lg:w-1/2 px-8">
+                        <div class="flex flex-col lg:w-1/2 px-8 h-screen overflow-scroll">
                             @include('partials.review.questions', compact('article'))
                         </div>
                         <div class="flex flex-col lg:w-1/2">
