2a9dac80f01590e277f7451dbed1084b3fd28b08
Haytham Mones <haythameyd@gmail.com>
2023-09-29 21:23:40 +0200

    Fixed issue with publishers statistics

--- app/Repositories/StatisticsRepository.php
+++ app/Repositories/StatisticsRepository.php
@@ -4,9 +4,7 @@ namespace App\Repositories;
 
 use App\Models\Publisher;
 use App\Models\PublisherScore;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Log;
 
 class StatisticsRepository
 {
@@ -21,17 +19,14 @@ class StatisticsRepository
 
         foreach (range(0, 11) as $count) {
             $month = now()->subYear()->firstOfYear()->addMonths($count);
-            $monthlyScore =  $monthlyScores->whereBetween('to', [
-                $month->startOfMonth()->format('Y-m-d h:m:i'),
-                $month->endOfMonth()->format('Y-m-d h:m:i')
-            ])->first();
+            $monthlyScore =  $monthlyScores->where('from', $month)->first();
 
-            array_push($scores, [
-                'score'   => $monthlyScore?->score ?? 100,
+            $scores[] = [
+                'score' => $monthlyScore?->score ?? 100,
                 'score_1' => $monthlyScore?->score_1 ?? 100,
                 'score_2' => $monthlyScore?->score_2 ?? 100,
                 'score_3' => $monthlyScore?->score_3 ?? 100,
-            ]);
+            ];
         }
 
         return collect($scores);
