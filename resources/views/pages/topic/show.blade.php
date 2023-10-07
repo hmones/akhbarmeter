d3d9c6d773e3d7be7421dc08d48d70aec4960717
Haytham Mones <haythameyd@gmail.com>
2023-10-07 11:54:46 +0200

    Fixed image issue for topics

--- resources/views/pages/topic/show.blade.php
+++ resources/views/pages/topic/show.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.default')
 @section('content')
-    <div style="background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('{{$topic->image ? Storage::url($topic->image) : ""}}'); background-position: center; background-size: cover;">
-        <div class="container flex flex-col w-full items-center justify-center mx-auto space-y-4 py-16 text-white">
+    <div style="background: linear-gradient(to right,#6dd5ed,#1850eb);">
+        <div class="container mx-auto flex flex-col w-full items-center justify-center mx-auto space-y-4 py-16 text-white">
             <div class="flex flex-col">
                 <h1 class="text-5xl leading-relaxed text-center font-extrabold tracking-tight">{{$topic->title}}</h1>
             </div>
             <div class="flex flex-row w-4/5 text-lg text-center leading-6 font-normal mx-auto justify-center space-x-1.5 rtl:space-x-reverse">
                 <span>{{$topic->created_at->format('d/m/Y')}} |</span>
-                <span class="flex flex-row rtl:flex-row-reverse items-center space-x-1.5 rtl:space-x-reverse">
+                <span class="flex flex-row items-center space-x-1.5 rtl:space-x-reverse">
                     <em class="fa fa-clock-o"></em>
                     <span> 5 {{translate('pages.topic.mins')}} |</span>
                 </span>
@@ -17,15 +17,16 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container mx-auto">
         <div class="flex flex-row items-center justify-center mx-auto space-y-10 my-16">
             <div>
+                <img src="{{Storage::url($topic->image)}}" alt="{{$topic->title}}" class="max-h-[400px] rounded float-left me-8" />
                 {!! $topic->description !!}
             </div>
         </div>
     </div>
 
-    <div class="container">
+    <div class="container mx-auto">
         <div class="flex flex-row items-center justify-center mx-auto space-y-4">
             <div class="container flex flex-col w-full items-center justify-center mx-auto space-y-4 py-16">
                 <div class="flex flex-col">
@@ -41,7 +42,7 @@
             </div>
         </div>
     </div>
-    <div class="container mb-10 space-y-10">
+    <div class="container mx-auto mb-10 space-y-10">
         @foreach($related->chunk(3) as $rowTopics)
             <div
                 class="flex flex-col xl:flex-row w-full items-start items-stretch justify-left mx-auto space-y-10 xl:space-y-0">
