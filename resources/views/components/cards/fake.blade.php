ff4ce4e922eaf52ae2dfea29ef9bcaf357c7c1d2
Haytham Mones <haythameyd@gmail.com>
2023-10-09 17:52:38 +0200

    Fixed the issue of fake news tags

--- resources/views/components/cards/fake.blade.php
+++ resources/views/components/cards/fake.blade.php
@@ -1,7 +1,9 @@
 @props(['article'])
 
 <div class="flex flex-col lg:flex-row justify-center w-full">
-    <div class="flex flex-col rounded-t-lg lg:rounded-none lg:ltr:rounded-l-lg rtl:lg:rounded-r-lg lg:w-1/3 bg-[url('{{$article?->image ? Storage::url($article->image) : asset('images/placeholders/article.png')}}')] bg-cover items-center justify-center h-[260px] lg:w-[470px]">
+    <div class="flex flex-col rounded-t-lg lg:rounded-none lg:ltr:rounded-l-lg rtl:lg:rounded-r-lg lg:w-1/3 bg-cover items-center justify-center h-[260px] lg:w-[470px]"
+         style="background-image: url('{{$article?->image ? Storage::url($article->image) : asset('images/placeholders/article.png')}}')"
+    >
         <img src="{{asset('images/icons/fake.svg')}}" alt="Fake article icon">
     </div>
     <div class="flex flex-col rounded-b-lg lg:rounded-none lg:ltr:rounded-r-lg lg:rtl:rounded-l-lg p-8 space-y-4 bg-gray-50 justify-content-start w-full">
@@ -10,7 +12,12 @@
                 @foreach(collect($article?->tags)->take(3) ?? [] as $tag)
                     <a href="#"
                        class="flex flex-row mx-2 px-2 bg-gray-100 h-fit rounded-5 text-sm mt-1">
-                        #{{data_get($tag, 'value', $tag)}}
+                        @if(is_array($tag))
+                            #{{data_get($tag, 'value', '')}}
+                        @endif
+                        @if($tag instanceof string)
+                            #{{$tag}}
+                        @endif
                     </a>
                 @endforeach
             </div>
