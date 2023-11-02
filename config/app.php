6ac85e01739c1002165dcc6411e693d3d764fadf
Haytham Mones <haythameyd@gmail.com>
2023-11-02 20:29:28 +0100

    Added Debugbar for fixing issues

--- config/app.php
+++ config/app.php
@@ -198,6 +198,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
         App\Providers\FacadeServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
