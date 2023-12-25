6c3bdeb910d5b123ae32b7420b68026aeff77e71
Haytham Mones <haythameyd@gmail.com>
2023-12-25 10:34:10 +0100

    Added links to publishers on top of the ranking

--- resources/views/components/home/rank-small.blade.php
+++ resources/views/components/home/rank-small.blade.php
@@ -1,8 +1,10 @@
 @props(['score'])
 <div class="flex flex-row space-x-2 rtl:space-x-reverse">
-    <img class="w-16 h-16"
-         src="{{$score?->publisher?->image ? Storage::url($score->publisher->image) : asset('images/placeholders/publisher.png')}}"
-         alt="{{$score?->publisher?->name}}"/>
+    <a href="{{ route('publishers.show', $score?->publisher?->id) }}">
+        <img class="w-16 h-16"
+             src="{{$score?->publisher?->image ? Storage::url($score->publisher->image) : asset('images/placeholders/publisher.png')}}"
+             alt="{{$score?->publisher?->name}}"/>
+    </a>
     <div class="flex flex-col space-y-1">
         <div>{{translate('components.home.rank.number')}} {{$score?->rank ?: 1}}</div>
         <x-trend :score="$score?->score ?: 100" :isTrending="$score?->is_trending ?: 1" percentageClasses="text-xl leading-7 font-semibold"/>
--- resources/views/components/home/rank.blade.php
+++ resources/views/components/home/rank.blade.php
@@ -1,9 +1,11 @@
 @props(['title' => 'Best last month', 'score'])
 <div class="flex flex-col items-center space-y-3">
     <div class="text-xs leading-4 font-semibold uppercase">{{$title}}</div>
-    <img class="w-28"
-         src="{{empty($score?->publisher?->image) ? asset('images/placeholders/publisher.png') : Storage::url($score->publisher->image)}}"
-         alt="{{$score?->publisher?->name}}"/>
+    <a href="{{ route('publishers.show', $score?->publisher?->id) }}">
+        <img class="w-28"
+             src="{{empty($score?->publisher?->image) ? asset('images/placeholders/publisher.png') : Storage::url($score->publisher->image)}}"
+             alt="{{$score?->publisher?->name}}"/>
+    </a>
     <x-trend :score="$score?->score ?? 100" :isTrending="$score?->is_trending ?? 1"/>
     <div class="text-xs leading-4 font-normal text-center">
         {{translate('components.home.rank.hint')}}
