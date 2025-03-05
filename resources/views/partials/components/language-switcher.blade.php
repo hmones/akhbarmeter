3898db9cb0f27e1ff69990851680a9411ea1310e
Maab Javaid <maabjavaid9@gmail.com>
2025-03-05 16:19:38 +0000

    Hide english option in language switcher

--- resources/views/partials/components/language-switcher.blade.php
+++ resources/views/partials/components/language-switcher.blade.php
@@ -1,7 +1,9 @@
-@if(app()->getLocale() == 'ar')
+{{--@if(app()->getLocale() == 'ar')
     <a href="{{str(url()->full())->before('?')->append('?lang=en')}}" class="font-['Inter']">English</a>
     <img src="{{asset('images/icons/us-flag.png')}}" class="w-auto h-6" alt="US Flag"/>
 @else
     <a href="{{str(url()->full())->before('?')->append('?lang=ar')}}" class="font-['Cairo']">عربي</a>
     <img src="{{asset('images/icons/eg-flag.png')}}" class="w-auto h-6" alt="Egyptian Flag"/>
-@endif
+@endif--}}
+<a href="{{str(url()->full())->before('?')->append('?lang=ar')}}" class="font-['Cairo']">عربي</a>
+<img src="{{asset('images/icons/eg-flag.png')}}" class="w-auto h-6" alt="Egyptian Flag"/>
