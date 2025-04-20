c95d72ab843b091cd6f464ee6263fa21da4065ba
Maab Javaid <maabjavaid9@gmail.com>
2025-04-20 15:42:00 +0100

    Add feature flag

--- config/feature-flags.php
+++ config/feature-flags.php
@@ -1,4 +1,5 @@
 <?php
 return [
     'akhbarmeter-in-media' => env('AKHBARMETER_IN_MEDIA', false),
+    'show-footer-menu' => env('SHOW_FOOTER_MENU', false),
 ];
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,15 @@
 <section id="footer" class="bg-gray-900 ">
     <div class="container flex flex-col mx-auto space-y-10 py-10 text-white">
-        <div class="flex flex-col lg:flex-row lg:justify-center items-center space-y-4 lg:space-y-0 rtl:space-x-reverse">
-            @foreach(translate('footer.menu') as $item)
-                <div class="px-0 lg:px-8">
-                    <a href="{{data_get($item, 'link')}}">{{data_get($item, 'text')}}</a>
-                </div>
-            @endforeach
-        </div>
-        <img src="{{asset('images/ifcn_badge.png')}}" class="flex flex-wrap w-[110px] mx-auto" alt="AkhbarMeter IFCN badge"/>
+        @if(config('feature-flags.show-footer-menu'))
+            <div class="flex flex-col lg:flex-row lg:justify-center items-center space-y-4 lg:space-y-0 rtl:space-x-reverse">
+                @foreach(translate('footer.menu') as $item)
+                    <div class="px-0 lg:px-8">
+                        <a href="{{data_get($item, 'link')}}">{{data_get($item, 'text')}}</a>
+                    </div>
+                @endforeach
+            </div>
+        @endif
+            <img src="{{asset('images/ifcn_badge.png')}}" class="flex flex-wrap w-[110px] mx-auto" alt="AkhbarMeter IFCN badge"/>
         <div class="flex justify-center space-x-4 rtl:space-x-reverse">
             @include('partials.components.social-media-links')
         </div>
