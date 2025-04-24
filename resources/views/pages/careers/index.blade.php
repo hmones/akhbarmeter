8e9fd4278a72b055a7ddfe4455766083114fd9f8
Maab Javaid <maabjavaid9@gmail.com>
2025-04-24 15:04:44 +0100

    Adjust topic type badges

--- resources/views/pages/careers/index.blade.php
+++ resources/views/pages/careers/index.blade.php
@@ -8,7 +8,7 @@
     <div class="container mx-auto px-4 py-10">
         <div class="text-center mb-8 max-w-2xl mx-auto">
             @if(count($careers) > 0)
-                <h1 class="text-4xl font-bold mb-4">{{ translate('pages.careers.header') }}</h1>
+                <h1 class="text-4xl font-bold mb-4">{{ translate('pages.careers.headerForJobs') }}</h1>
             @else
                 <h1 class="text-4xl font-bold mb-4">{{ translate('pages.careers.headerWithNoJob') }}</h1>
             @endif
