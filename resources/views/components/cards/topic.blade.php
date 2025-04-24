c8b8a4ba9849bc9eb3b91014f8e613dd16b4a228
Maab Javaid <maabjavaid9@gmail.com>
2025-04-24 14:11:49 +0100

    Adjust fake news badges

--- resources/views/components/cards/topic.blade.php
+++ resources/views/components/cards/topic.blade.php
@@ -7,15 +7,15 @@
                 <!-- Even Larger Badge Image -->
                 @if($topic->type === 'fakeNews')
                     @if($topic->fake_news_badge === 'notTrue')
-                        <img src="{{ asset('images/topic/badges/fake-news/Untrue.png') }}" alt="Badge" class="absolute top-2 right-2 w-20 h-18">
+                        <img src="{{ asset('images/topic/badges/fake-news/Untrue.png') }}" alt="Badge" class="absolute top-4 left-4 w-21 h-18">
                     @elseif($topic->fake_news_badge === 'halfTrue')
-                        <img src="{{ asset('images/topic/badges/fake-news/Half-ture.png') }}" alt="Badge" class="absolute top-1 right-2 w-20 h-18">
+                        <img src="{{ asset('images/topic/badges/fake-news/Half-ture.png') }}" alt="Badge" class="absolute top-4 left-4 w-20 h-18">
                     @elseif($topic->fake_news_badge === 'misleadingContext')
-                        <img src="{{ asset('images/topic/badges/fake-news/Misleading.png') }}" alt="Badge" class="absolute top-1 right-2 w-20 h-18">
+                        <img src="{{ asset('images/topic/badges/fake-news/Misleading.png') }}" alt="Badge" class="absolute top-4 left-4 w-20 h-18">
                     @elseif($topic->fake_news_badge === 'unproven')
-                        <img src="{{ asset('images/topic/badges/fake-news/Unproven.png') }}" alt="Badge" class="absolute top-2 right-2 w-20 h-18">
+                        <img src="{{ asset('images/topic/badges/fake-news/Unproven.png') }}" alt="Badge" class="absolute top-4 left-4 w-20 h-18">
                     @else
-                        <img src="{{ asset('images/topic/badges/fake-news/True.png') }}" alt="Badge" class="absolute top-2 right-2 w-20 h-18">
+                        <img src="{{ asset('images/topic/badges/fake-news/True.png') }}" alt="Badge" class="absolute top-4 left-4 w-20 h-18">
                     @endif
                 @elseif($topic->type === 'akhbarMeterInMedia')
                     <img src="{{ asset('images/topic/badges/AkhbarMeter_in_Media.png') }}" alt="Badge" class="absolute top-2 right-2 w-23 h-20">
