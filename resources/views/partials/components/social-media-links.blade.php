eaaa9f1cc14d36baaae2e018b905b6928eee6a66
Maab Javaid <maabjavaid9@gmail.com>
2025-04-24 15:34:19 +0100

    Add new social media icons

--- resources/views/partials/components/social-media-links.blade.php
+++ resources/views/partials/components/social-media-links.blade.php
@@ -7,6 +7,15 @@
 <a href="https://www.twitter.com/akhbarmeter" target="_blank">
     <img src="{{asset('images/icons/x.svg')}}" alt="x icon">
 </a>
+<a href="https://www.youtube.com/@akhbarmeter" target="_blank">
+    <img src="{{asset('images/icons/youtube.svg')}}" alt="x icon">
+</a>
+<a href="https://www.tiktok.com/@akhbarmeter" target="_blank">
+    <img src="{{asset('images/icons/tiktok.svg')}}" alt="x icon">
+</a>
+<a href="https://www.linkedin.com/in/akhbarmeter" target="_blank">
+    <img src="{{asset('images/icons/linkedin.svg')}}" alt="x icon">
+</a>
 <a href="mailto:info@akhbarmeter.org" target="_blank">
     <img src="{{asset('images/icons/mail.svg')}}" alt="mail icon">
 </a>
