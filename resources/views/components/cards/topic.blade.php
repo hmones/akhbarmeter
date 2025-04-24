f5f201a5479d960a2f0e923d1d293f2dd84335cf
Maab Javaid <maabjavaid9@gmail.com>
2025-04-24 12:45:29 +0100

    Add badges for topic types

--- resources/views/components/cards/topic.blade.php
+++ resources/views/components/cards/topic.blade.php
@@ -17,6 +17,22 @@
                     @else
                         <img src="{{ asset('images/topic/badges/fake-news/True.png') }}" alt="Badge" class="absolute top-2 right-2 w-20 h-18">
                     @endif
+                @elseif($topic->type === 'akhbarMeterInMedia')
+                    <img src="{{ asset('images/topic/badges/AkhbarMeter_in_Media.png') }}" alt="Badge" class="absolute top-2 right-2 w-23 h-20">
+                @elseif($topic->type === 'codeOfEthics')
+                    <img src="{{ asset('images/topic/badges/Code_of_Ethics.png') }}" alt="Badge" class="absolute top-2 right-2 w-23 h-20">
+                @elseif($topic->type === 'explainer')
+                    <img src="{{ asset('images/topic/badges/Explainer.png') }}" alt="Badge" class="absolute top-2 right-2 w-23 h-20">
+                @elseif($topic->type === 'factSheet')
+                    <img src="{{ asset('images/topic/badges/Factsheet.png') }}" alt="Badge" class="absolute top-2 right-2 w-23 h-20">
+                @elseif($topic->type === 'mediaAnalysisReports')
+                    <img src="{{ asset('images/topic/badges/Media_analysis_reports.png') }}" alt="Badge" class="absolute top-2 right-2 w-23 h-20">
+                @elseif($topic->type === 'other')
+                    <img src="{{ asset('images/topic/badges/Other.png') }}" alt="Badge" class="absolute top-2 right-2 w-23 h-20">
+                @elseif($topic->type === 'violations')
+                    <img src="{{ asset('images/topic/badges/Violations.png') }}" alt="Badge" class="absolute top-2 right-2 w-23 h-20">
+                @else
+                    <img src="{{ asset('images/topic/badges/Workshop.png') }}" alt="Badge" class="absolute top-2 right-2 w-23 h-20">
                 @endif
             </div>
         </a>
